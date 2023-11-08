484eb9af465b94d6ab63137ea7a543c333eebc67
Maqsadbek <110594640+MAQSADBEK77@users.noreply.github.com>
2023-11-08 12:59:02 +0500

    Upload chat image

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-
+import chatIcon from "../../public/icons/mail.svg";
 function Chat() {
   return (
     <Link to="/chat" className="chat-btn">
-      <img src="./public/icons/mail.svg" className="mailIcon w-6 h-6" />
+      <img src={chatIcon} className="mailIcon w-6 h-6" />
     </Link>
   );
 }
